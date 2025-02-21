3c303a6bf54a21130122c2ad7b1b47b53b3b5426
sanjay_n_kadur <sanjaynkadur@gmail.com>
2025-02-21 10:48:02 +0530

    added header

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<?php include 'includes/header.php'; ?>
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/index.css">
+
+<title>Optimum Sync</title>
+</head>
+
+<body>
+
+
+
+
+
+
+<!-- ?php include 'includes/footer.php'; ?> -->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
+
+<script src="scripts/index.js"></script>
+
+
+</body>
